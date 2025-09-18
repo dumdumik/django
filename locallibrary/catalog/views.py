a43899ff0994917507ac5a75b8128e7a0e4d5e3a
dumdumik <fjdvgt149@gmail.com>
2025-09-18 15:35:43 +0700

    home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    search_word =  request.GET.get('search_word', ' ')
+    search_word_lower = search_word.lower()
+
+    if search_word:
+        book_count = Book.objects.filter(title__icontains=search_word_lower).count()
+    else:
+        book_count = 0
+
+    return render (
+        request,
+        'index.html',
+        context=
+        {'num_books':num_books,'num_instances':num_instances, 'num_instances_available':num_instances_available,'num_authors':num_authors, 'search_word':search_word, 'book_count':book_count},
+    )
+
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-urlpatterns = [   path('admin/', admin.site.urls),
+urlpatterns = [
+path('admin/', admin.site.urls),
 path('catalog/', include('catalog.urls')),path('',
 RedirectView.as_view(url='/catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
