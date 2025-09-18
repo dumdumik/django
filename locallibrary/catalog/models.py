829ce36c26e32e3ef61b897b91fd282662561e8a
dumdumik <fjdvgt149@gmail.com>
2025-09-18 15:33:55 +0700

    admin

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -17,6 +17,9 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    def display_genre(self):
+        return ','.join([ genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         return self.title
@@ -26,7 +29,7 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="Unique ID for this particular book across whole library")
+                          help_text="Unique ID for this particular book across the whole library")
 
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
@@ -41,11 +44,14 @@ class BookInstance(models.Model):
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m',
                               help_text="Book availability")
+
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
-        return '%s (%s)' % (self.id, self.book.title)
+        return f'{self.book.title} (ID: {self.id}) - Status: {self.get_status_display()}, Due back: {self.due_back if self.due_back else "No due date"}'
+
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
